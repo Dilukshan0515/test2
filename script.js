6cf30377ce3818f4cdf4ebab5ac22689031c8e21
Dilukshan0515 <dilu030227@gmail.com>
2026-05-02 20:42:35 +0530

    modfied js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Simple JS example
+
+// variable
+let name = "Dilu";
+
+// function
+function greet(user) {
+    console.log("Hello, " + user + "!");
+}
+
+// call function
+greet(name);
+
+// simple condition
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
